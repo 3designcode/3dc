d25a07515ad309a1c3a7d3da2263a3fc1d1ce99a
3designcode <3designcode@gmail.com>
2024-11-06 18:18:25 +0800

    Button update

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Button as="a" 
                   href="mailto:yourworkemail@example.com?subject=Work with Us Inquiry"
-                  className="px-6 py-3 text-white bg-blue-600 hover:bg-blue-700 rounded-md">
+                  className="px-6 py-3 text-white rounded-md">
                     Work With Us
           </Button>
         </div>
